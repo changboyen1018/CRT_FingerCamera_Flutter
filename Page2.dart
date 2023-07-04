db408302175a543ac2e1488948681004eea08f90
Bo-Yen (Danny) Chang <138422608+changboyen1018@users.noreply.github.com>
2023-07-04 17:11:13 +0800

    Create Page2.dart

---
+++ Page2.dart
@@ -0,0 +1,227 @@
+// Ezgif1 替換成影片模型
+
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class 0607cameraviewWidget extends StatelessWidget {
+          @override
+          Widget build(BuildContext context) {
+          // Figma Flutter Generator 0607cameraviewWidget - FRAME
+            return Container(
+      width: 375,
+      height: 812,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(0, 0, 0, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 678,
+        left: 212,
+        child: Transform.rotate(
+        angle: -0.000005008956538086317 * (math.pi / 180),
+        child: Divider(
+        color: Color.fromRGBO(84, 84, 88, 1),
+        thickness: 1
+      )
+      ,
+      )
+      ),Positioned(
+        top: 654,
+        left: 20,
+        child: Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(24),
+            topRight: Radius.circular(24),
+            bottomLeft: Radius.circular(24),
+            bottomRight: Radius.circular(24),
+          ),
+      color : Color.fromRGBO(120, 120, 128, 0.36000001430511475),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('􀙨', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'SF Pro',
+        fontSize: 24,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1.5 /*PERCENT not supported*/
+      ),),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 654,
+        left: 164,
+        child: Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(120),
+            topRight: Radius.circular(120),
+            bottomLeft: Radius.circular(120),
+            bottomRight: Radius.circular(120),
+          ),
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 69, 58, 1),
+      border : Border.all(
+          color: Color.fromRGBO(0, 0, 0, 1),
+          width: 4,
+        ),
+      borderRadius : BorderRadius.all(Radius.elliptical(40, 40)),
+  )
+      ),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 203,
+        left: -1,
+        child: Container(
+        width: 376,
+        height: 291,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Ezgif1.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 145,
+        left: 84,
+        child: Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(4),
+            topRight: Radius.circular(4),
+            bottomLeft: Radius.circular(4),
+            bottomRight: Radius.circular(4),
+          ),
+      color : Color.fromRGBO(9, 196, 50, 1),
+      border : Border.all(
+          color: Color.fromRGBO(88, 255, 135, 1),
+          width: 1,
+        ),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('🙆‍♂️ Auto FingerAnalysis', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'ABeeZee',
+        fontSize: 18,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1.1111111111111112
+      ),),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 654,
+        left: 310,
+        child: Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(120),
+            topRight: Radius.circular(120),
+            bottomLeft: Radius.circular(120),
+            bottomRight: Radius.circular(120),
+          ),
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+      border : Border.all(
+          color: Color.fromRGBO(0, 0, 0, 1),
+          width: 2,
+        ),
+      borderRadius : BorderRadius.all(Radius.elliptical(40, 40)),
+  )
+      ),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 544,
+        left: 103,
+        child: Text('Place your finger', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'ABeeZee',
+        fontSize: 22,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1.2727272727272727
+      ),)
+      ),Positioned(
+        top: 713,
+        left: 163,
+        child: Text('Auto', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'ABeeZee',
+        fontSize: 22,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1.2727272727272727
+      ),)
+      ),Positioned(
+        top: 572,
+        left: 105,
+        child: Text('at front Camera', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'ABeeZee',
+        fontSize: 22,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1.2727272727272727
+      ),)
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 778,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),
+        ]
+      )
+    );
+          }
+        }
+
+
+// Ezgif1 替換成影片模型
