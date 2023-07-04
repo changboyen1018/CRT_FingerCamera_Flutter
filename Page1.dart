f2dd7a7a466e4b713c7e2b3db1b820813b543666
Bo-Yen (Danny) Chang <138422608+changboyen1018@users.noreply.github.com>
2023-07-04 17:09:51 +0800

    Create Page1.dart

---
+++ Page1.dart
@@ -0,0 +1,289 @@
+import 'package:flutter/material.dart';
+
+
+class 0601cameraviewWidget extends StatelessWidget {
+          @override
+          Widget build(BuildContext context) {
+          // Figma Flutter Generator 0601cameraviewWidget - FRAME
+            return Container(
+      width: 375,
+      height: 812,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(0, 0, 0, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 508,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 72,
+        left: 0,
+        child: Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('Place your finger', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'ABeeZee',
+        fontSize: 22,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1.2727272727272727
+      ),),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 72,
+        left: 0,
+        child: Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('Place your finger', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'ABeeZee',
+        fontSize: 22,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1.2727272727272727
+      ),),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 72,
+        left: 0,
+        child: Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('Place your finger', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'ABeeZee',
+        fontSize: 22,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1.2727272727272727
+      ),),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 189,
+        left: 105,
+        child: Text('at front Camera', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'ABeeZee',
+        fontSize: 22,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1.2727272727272727
+      ),)
+      ),Positioned(
+        top: 640,
+        left: 150,
+        child: Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+      width: 55,
+      height: 56,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(100),
+            topRight: Radius.circular(100),
+            bottomLeft: Radius.circular(100),
+            bottomRight: Radius.circular(100),
+          ),
+      border : Border.all(
+          color: Color.fromRGBO(84, 84, 88, 1),
+          width: 1,
+        ),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 4, vertical: 1),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('􀎡', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 1),
+        fontFamily: 'SF Pro',
+        fontSize: 18,
+        letterSpacing: -0.4300000071525574,
+        fontWeight: FontWeight.normal,
+        height: 1.5 /*PERCENT not supported*/
+      ),),
+
+        ],
+      ),
+    )
+      ),
+        ]
+      )
+    ),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 268,
+        left: 21,
+        child: Container(
+        width: 335,
+        height: 160,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Group117.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 504,
+        left: 84,
+        child: SvgPicture.asset(
+        'assets/images/rectangle301.svg',
+        semanticsLabel: 'rectangle301'
+      );
+      ),Positioned(
+        top: 496,
+        left: 108,
+        child: Container(
+      decoration: BoxDecoration(
+          boxShadow : [BoxShadow(
+          color: Color.fromRGBO(0, 0, 0, 0.25),
+          offset: Offset(0,4),
+          blurRadius: 4
+      )],
+      border : Border.all(
+          color: Color.fromRGBO(0, 0, 0, 1),
+          width: 1,
+        ),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(4),
+            topRight: Radius.circular(4),
+            bottomLeft: Radius.circular(4),
+            bottomRight: Radius.circular(4),
+          ),
+      color : Color.fromRGBO(9, 196, 50, 1),
+      border : Border.all(
+          color: Color.fromRGBO(88, 255, 135, 1),
+          width: 4,
+        ),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('🙆‍♂️ Click to Start Recording ', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'ABeeZee',
+        fontSize: 18,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1.1111111111111112
+      ),),
+
+        ],
+      ),
+    ),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 778,
+        left: 0,
+        child: Container(
+      width: 375,
+      height: 34,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: Container(
+      width: 375,
+      height: 34,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),
+        ]
+      )
+    )
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: Container(
+        width: 375,
+        height: 44,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Statusbar.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),
+        ]
+      )
+    );
+          }
+        }
