33563daa653aeab5367018efc6d120add65cf075
Bo-Yen (Danny) Chang <138422608+changboyen1018@users.noreply.github.com>
2023-07-04 17:12:27 +0800

    Create Page4.dart

---
+++ Page4.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+
+
+class 0609cameraviewWidget extends StatelessWidget {
+          @override
+          Widget build(BuildContext context) {
+          // Figma Flutter Generator 0609cameraviewWidget - FRAME
+            return Container(
+      width: 375,
+      height: 812,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(0, 0, 0, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 654,
+        left: 20,
+        child: Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(24),
+            topRight: Radius.circular(24),
+            bottomLeft: Radius.circular(24),
+            bottomRight: Radius.circular(24),
+          ),
+      color : Color.fromRGBO(120, 120, 128, 0.36000001430511475),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('􀙨', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'SF Pro',
+        fontSize: 24,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1.5 /*PERCENT not supported*/
+      ),),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 654,
+        left: 164,
+        child: Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(120),
+            topRight: Radius.circular(120),
+            bottomLeft: Radius.circular(120),
+            bottomRight: Radius.circular(120),
+          ),
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 69, 58, 1),
+      border : Border.all(
+          color: Color.fromRGBO(0, 0, 0, 1),
+          width: 4,
+        ),
+      borderRadius : BorderRadius.all(Radius.elliptical(40, 40)),
+  )
+      ),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 654,
+        left: 307,
+        child: Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(120),
+            topRight: Radius.circular(120),
+            bottomLeft: Radius.circular(120),
+            bottomRight: Radius.circular(120),
+          ),
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+      border : Border.all(
+          color: Color.fromRGBO(0, 0, 0, 1),
+          width: 2,
+        ),
+      borderRadius : BorderRadius.all(Radius.elliptical(40, 40)),
+  )
+      ),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 67,
+        left: 5,
+        child: Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(4),
+            topRight: Radius.circular(4),
+            bottomLeft: Radius.circular(4),
+            bottomRight: Radius.circular(4),
+          ),
+      color : Color.fromRGBO(255, 69, 58, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('Analyzing....', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'ABeeZee',
+        fontSize: 15,
+        letterSpacing: 0,
+        fontWeight: FontWeight.normal,
+        height: 1.3333333333333333
+      ),),
+
+        ],
+      ),
+    ),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 252,
+        left: -1,
+        child: null
+      ),Positioned(
+        top: 328,
+        left: 103,
+        child: Container(
+        width: 161,
+        height: 62,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/3f2c979b214d06e9caab8ba8326864f31.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 778,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),
+        ]
+      )
+    );
+          }
+        }
+
+
+
+// gif 如下
+// 3f2c979b214d06e9caab8ba8326864f31
